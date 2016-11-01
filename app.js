97b1c167c5bac0eec7c111a5f37fcdb490cf48dc
Tae Hee Kim <thkim93@yahoo.com>
2016-10-31 21:02:54 -0700

    more fixes. not quite there yet

--- app.js
+++ app.js
@@ -12,14 +12,15 @@ var app = express();
 
 // view engine setup
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname + 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@
 var express = require('express');
 var router = express.Router();
 
-exports.view = function(req, res){
+router.get('/', function(req, res, next){
   res.render('index');
-};
+});
 
 module.exports = router;
